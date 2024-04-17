86577518c8131c2a3fab5b2a20dc3f4ec1d09f69
zhouhengchao <zhouhengchao@yzw.cn>
2024-04-17 23:03:51 +0800

    Revert "测试暂存使用"
    This reverts commit 0ecbd50a5e7d569f3b9f9c4c404afcaf54216e87.

--- src/main/java/com/zhc/git/GitTest.java
+++ src/main/java/com/zhc/git/GitTest.java
@@ -5,8 +5,4 @@ public class GitTest {
     public void test1(){
         System.out.println("测试使用git stage");
     }
-
-    public void test2(){
-        System.out.println("测试使用git stage1");
-    }
 }
